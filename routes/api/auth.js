b693416d5fb6b6b4cce56026968eb4bbcbfe96e5
MiraDov290 <muzhyk.myroslava@gmail.com>
2023-11-22 14:58:43 +0000

    chang

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -17,10 +17,10 @@ router.get("/current", authenticate, ctrl.getCurrent);
 router.post("/logout", authenticate, ctrl.logout);
 
 router.patch(
-    "/avatars",
-    authenticate,
-    upload.single("avatar"),
-    ctrl.updateAvatar
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  ctrl.updateAvatar
 );
 
 module.exports = router;
